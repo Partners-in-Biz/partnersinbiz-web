c3106b6be0e2d9f1a7324c418971f92bc5a268e5
Peet Stander <peet.stander@partnersinbiz.online>
2026-06-04 12:48:37 +0000

    feat(admin): add Peet mission control

---
+++ app/(admin)/admin/mission-control/page.tsx
@@ -0,0 +1,7 @@
+import { PeetMissionControl } from '@/components/mission-control/PeetMissionControl'
+
+export const dynamic = 'force-dynamic'
+
+export default function AdminMissionControlPage() {
+  return <PeetMissionControl />
+}
